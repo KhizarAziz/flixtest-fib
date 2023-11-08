bc38030ead09c99b39c3e6a34796a32f17da1d12
KhizarAziz <khizar@scanbot.io>
2023-11-08 20:43:52 +0100

    improved  main

--- app/main.py
+++ app/main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
-def fibonacci(n):
+def fibonacci(n: int) -> int:
+    """
+    Calculate the n-th Fibonacci number.
+
+    Parameters:
+    n (int): The index (n) of the Fibonacci sequence to compute.
+
+    Returns:
+    int: The n-th Fibonacci number.
+    """
+    if n < 0:
+        raise ValueError("Input must be a non-negative integer.")
+    
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
     return a
 
-@app.get("/{n}")
-async def read_item(n: int):
-    return fibonacci(n)
+@app.get("/{n}", response_model=int)
+async def read_item(n: int) -> int:
+    """
+    Endpoint to retrieve the n-th Fibonacci number.
+
+    Parameters:
+    n (int): The index (n) of the Fibonacci sequence to retrieve.
+
+    Responses:
+    200: Return the n-th Fibonacci number.
+    422: Validation error for invalid input.
+    """
+    try:
+        return fibonacci(n)
+    except ValueError as e:
+        raise HTTPException(status_code=422, detail=str(e))
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,18 +1,38 @@
 from fastapi.testclient import TestClient
 from app.main import app
 
-client = TestClient(app)
+# Utilize a fixture for the TestClient to ensure it's reusable and not hard-coded.
+@pytest.fixture(scope="module")
+def test_client():
+    with TestClient(app) as client:
+        yield client
 
-def test_read_main():
+def test_fibonacci_sequence(test_client):
+    """
+    Test the Fibonacci API for known inputs and outputs.
+    
+    This test checks if the API returns the correct Fibonacci sequence 
+    for the given input numbers. It uses a parameterized test approach
+    to avoid redundancy and makes the test easily extendable.
+    
+    Args:
+        test_client (TestClient): Fixture for creating a test client for the API.
+    """
 
-    response = client.get("/1")
-    assert response.status_code == 200
-    assert response.json() == 1
+    # Dictionary mapping input values to expected Fibonacci results
+    test_cases = {
+        1: 1,
+        3: 2,
+        7: 13
+    }
 
-    response = client.get("/3")
-    assert response.status_code == 200
-    assert response.json() == 2
-
-    response = client.get("/7")
-    assert response.status_code == 200
-    assert response.json() == 13
+    # Loop through the test cases to ensure code reusability
+    for input_value, expected_result in test_cases.items():
+        # Make the GET request to the FastAPI endpoint
+        response = test_client.get(f"/{input_value}")
+        
+        # Check if the status code is as expected (200 OK)
+        assert response.status_code == 200, f"Failed for input: {input_value}"
+        
+        # Check if the JSON response is the expected Fibonacci number
+        assert response.json() == expected_result, f"Failed for input: {input_value}"
