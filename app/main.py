db73270662220d9190e6e66ce05062d9437146d1
KhizarAziz <khizar@scanbot.io>
2023-11-08 21:16:15 +0100

    fixed codde

--- app/main.py
+++ app/main.py
@@ -14,7 +14,8 @@ def fibonacci(n: int) -> int:
     """
     if n < 0:
         raise ValueError("Input must be a non-negative integer.")
-    
+    if n == 0:
+        return 0
     a, b = 0, 1
     for _ in range(n):
         a, b = b, a + b
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,8 @@
+import pytest
 from fastapi.testclient import TestClient
 from app.main import app
 
-# Utilize a fixture for the TestClient to ensure it's reusable and not hard-coded.
+# Utilize a fixture for the TestClient
 @pytest.fixture(scope="module")
 def test_client():
     with TestClient(app) as client:
@@ -21,9 +22,13 @@ def test_fibonacci_sequence(test_client):
 
     # Dictionary mapping input values to expected Fibonacci results
     test_cases = {
+        0:0,
         1: 1,
+        2: 1,
         3: 2,
-        7: 13
+        7: 13,
+        10: 55,
+        20: 6765
     }
 
     # Loop through the test cases to ensure code reusability
